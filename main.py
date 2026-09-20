0f72e5683a8c952af19a2047e72b3a1080c2a558
aryansanchiya <asanchiya@gmail.com>
2026-09-20 15:32:27 +0530

    CORS ADDED

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from api.v1.tenant import router as tenant_router
 from api.v1.user import router as user_router
 from api.v1.auth import router as auth_router
 from api.v1.crud_city import router as crud_city_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="CivicAlert API",
@@ -16,6 +17,15 @@ def health():
         "status": "healthy"
     }
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(tenant_router)
 app.include_router(user_router)
 app.include_router(auth_router)
